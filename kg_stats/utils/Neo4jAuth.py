0f6c1f3498549baf3cdd5bc01af6d61097b37a1a
Walke <daniel.walke@isas.de>
2026-03-13 13:29:52 +0100

    rmeoved unnnecessary files

--- kg_stats/utils/Neo4jAuth.py
+++
@@ -1,5 +0,0 @@
-class Neo4jAuth:
-    def __init__(self, uri = "bolt://localhost:7687", user = "neo4j", password = "password"):
-        self.uri = uri
-        self.user =  user
-        self.password = password
