9c13e12976439747d99754706d856a3bbcdb9bb6
Daniel Walke <daniel.walke@ovgu.de>
2026-04-17 08:02:00 +0200

    more specific cleaning

--- utils/docker_cleanup/clean_neo4j_docker.sh
+++ utils/docker_cleanup/clean_neo4j_docker.sh
@@ -1,1 +1,4 @@
-docker stop $(docker ps -q --filter ancestor=neo4j:latest) && docker rm $(docker ps -aq --filter ancestor=neo4j:latest)
+docker stop neo4j-postfiltered-graph && docker rm neo4j-postfiltered-graph
+docker stop neo4j-postfiltered-metagraph && docker rm neo4j-postfiltered-metagraph
+docker stop neo4j-filtered-rolledup-metagraph && docker rm neo4j-filtered-rolledup-metagraph
+docker stop filtered_rolledup_graph && docker rm filtered_rolledup_graph
