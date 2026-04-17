0ace008afee0243c4e1c4a54443243d114f8fcaa
Daniel Walke <daniel.walke@ovgu.de>
2026-04-17 08:01:25 +0200

    concept creation for now without proper id matching

--- s1_raw_graph/add_new_concepts_with_mappings.py
+++ s1_raw_graph/add_new_concepts_with_mappings.py
@@ -33,7 +33,6 @@ def setup_concept_indexes(driver):
                 """
             ).consume()
 
-
 driver = GraphDatabase.driver("bolt://localhost:8083", auth=None)
 
 setup_concept_indexes(driver)
@@ -42,7 +41,7 @@ with open("config/s1_raw_graph/database_mapping_identifiers.json", "r") as f:
     concept_to_databases_with_mappings = json.load(f)
 
 for concept, mapping_info in concept_to_databases_with_mappings.items():
-    if concept in ["UNCLASSIFIED", "METADATA", "GENOMIC_REPEAT"]:
+    if concept in ["UNCLASSIFIED", "METADATA", "GENOMIC_REPEAT", "SEQUENCE_FEATURE"]:
         print(f"Skipping concept '{concept}' as it is not a valid concept for mapping in my honest opinion.")
         continue
     
@@ -57,31 +56,46 @@ for concept, mapping_info in concept_to_databases_with_mappings.items():
             
             final_prop_name = db_config['final_property_name']
             
-            session.run(
-                f"""
-                MATCH (db_node:{db})
-                WHERE db_node.`{final_prop_name}` IS NOT NULL
-                WITH db_node.`{final_prop_name}` AS raw_val, collect(db_node) AS db_nodes
-                CALL {{
-                    WITH raw_val, db_nodes
-                    WITH raw_val, db_nodes, toLower(raw_val) AS search_val
-                    OPTIONAL MATCH (c:{concept})
-                    WHERE search_val IN c.search_names OR search_val IN c.search_ids
-                    WITH raw_val, search_val, db_nodes, collect(c) AS matched_concepts
-                    FOREACH (existing_c IN matched_concepts |
-                        SET existing_c.names = CASE WHEN NOT raw_val IN coalesce(existing_c.names, []) THEN coalesce(existing_c.names, []) + [raw_val] ELSE coalesce(existing_c.names, []) END,
-                            existing_c.search_names = CASE WHEN NOT search_val IN coalesce(existing_c.search_names, []) THEN coalesce(existing_c.search_names, []) + [search_val] ELSE coalesce(existing_c.search_names, []) END
-                        FOREACH (node IN db_nodes | MERGE (node)-[:MAPPED_TO]->(existing_c))
-                    )
-                    FOREACH (_ IN CASE WHEN size(matched_concepts) = 0 THEN [1] ELSE [] END |
-                        CREATE (new_c:{concept} {{
-                            names: [raw_val], 
-                            ids: [], 
-                            search_names: [search_val], 
-                            search_ids: []
-                        }})
-                        FOREACH (node IN db_nodes | MERGE (node)-[:MAPPED_TO]->(new_c))
-                    )
-                }} IN TRANSACTIONS OF 500 ROWS
-                """
-            ).consume()
+            session.run(f"""
+            MATCH (db_node:{db})
+WHERE db_node.`{final_prop_name}` IS NOT NULL AND NOT EXISTS((db_node)-[:MAPPED_TO]->())
+CALL {{
+    WITH db_node
+    CREATE (new_c:{concept} {{
+        names: [db_node.`{final_prop_name}`], 
+        ids: [], 
+        search_names: [toLower(db_node.`{final_prop_name}`)], 
+        search_ids: []
+    }})
+    CREATE (db_node)-[:MAPPED_TO]->(new_c)
+}} IN TRANSACTIONS OF 5000 ROWS
+            """).consume()
+
+            # session.run(
+            #     f"""
+            #     MATCH (db_node:{db})
+            #     WHERE db_node.`{final_prop_name}` IS NOT NULL AND NOT EXISTS((db_node)-[:MAPPED_TO]->())
+            #     WITH db_node.`{final_prop_name}` AS raw_val, collect(db_node) AS db_nodes
+            #     CALL {{
+            #         WITH raw_val, db_nodes
+            #         WITH raw_val, db_nodes, toLower(raw_val) AS search_val
+            #         OPTIONAL MATCH (c:{concept})
+            #         WHERE search_val IN c.search_names OR search_val IN c.search_ids
+            #         WITH raw_val, search_val, db_nodes, collect(c) AS matched_concepts
+            #         FOREACH (existing_c IN matched_concepts |
+            #             SET existing_c.names = CASE WHEN NOT raw_val IN coalesce(existing_c.names, []) THEN coalesce(existing_c.names, []) + [raw_val] ELSE coalesce(existing_c.names, []) END,
+            #                 existing_c.search_names = CASE WHEN NOT search_val IN coalesce(existing_c.search_names, []) THEN coalesce(existing_c.search_names, []) + [search_val] ELSE coalesce(existing_c.search_names, []) END
+            #             FOREACH (node IN db_nodes | MERGE (node)-[:MAPPED_TO]->(existing_c))
+            #         )
+            #         FOREACH (_ IN CASE WHEN size(matched_concepts) = 0 THEN [1] ELSE [] END |
+            #             CREATE (new_c:{concept} {{
+            #                 names: [raw_val], 
+            #                 ids: [], 
+            #                 search_names: [search_val], 
+            #                 search_ids: []
+            #             }})
+            #             FOREACH (node IN db_nodes | MERGE (node)-[:MAPPED_TO]->(new_c))
+            #         )
+            #     }} IN TRANSACTIONS OF 5000 ROWS
+            #     """
+            # ).consume()
