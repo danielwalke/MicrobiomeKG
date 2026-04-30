19fae7c05b6f8445bca9fe26c207d9dc79b3fbe4
Daniel Walke <daniel.walke@ovgu.de>
2026-04-30 09:30:56 +0200

    disease concept extended to disease database

--- mapping/integrations/disease.py
+++ mapping/integrations/disease.py
@@ -1,15 +1,17 @@
 from pydantic import Field
 from typing import List
-from mapping.models import HprdDisease
+from mapping.models import HPRD_Disease, DISEASES_Disease
 from mapping.integrations.base_merged import BaseMergedEntity
 
 class MergedDisease(BaseMergedEntity):
-    __label__ = "MergedDisease"
+    __label__ = "DISEASE"
     __source_mappings__ = {
-        "hprd_names": (HprdDisease, "name"),
+        "hprd_names": (HPRD_Disease, "name"),
+        "diseases_names": (DISEASES_Disease, "name")
     }
 
     hprd_names: List[str] = Field(default_factory=list)
+    diseases_names: List[str] = Field(default_factory=list)
 
     mapped: bool = True
     merged_ids: List[str] = Field(default_factory=list)
@@ -24,8 +26,11 @@ class MergedDisease(BaseMergedEntity):
         """The actual processing logic that runs inside the Integrator."""
         if self.hprd_names:
             self.hprd_names = [f"{id}" for id in self.hprd_names if id is not None]
+        if self.diseases_names:
+            self.diseases_names = [f"{id}" for id in self.diseases_names if id is not None]
 
     def integrate(self):
         unique_ids = set()
         unique_ids.update(self.hprd_names)
+        unique_ids.update(self.diseases_names)
         self.merged_ids = list(unique_ids)
