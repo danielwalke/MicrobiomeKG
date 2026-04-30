d5596e9ae41ea4c269b93c59ef0601f28aa8dc4c
Daniel Walke <daniel.walke@ovgu.de>
2026-04-30 09:32:30 +0200

    mapping only to extend biodwh2 mappings

--- mapping/integrations/main.py
+++ mapping/integrations/main.py
@@ -1,29 +1,31 @@
-
-from neo4j import GraphDatabase
 from mapping.integrations.integrator import NodeIntegrator
-from mapping.integrations.publication import MergedPublication
-from mapping.integrations.gene import MergedGene
-from mapping.integrations.rna import MergedRna
 from mapping.integrations.protein_domain import MergedProteinDomain
 from mapping.integrations.entity_resolver import EntityResolver
 from mapping.integrations.connector import Neo4jConnector
-from mapping.integrations.protein import MergedProtein
-from mapping.integrations.taxon import MergedTaxon
 from mapping.integrations.term import MergedTerm
 from mapping.integrations.disease import MergedDisease
 from mapping.integrations.tissue import MergedTissue
 from mapping.integrations.ptm import MergedPTM
 
+
+def delete_label_in_batches(session, label, batch_size):
+    query = f"""
+    MATCH (n:{label})
+    CALL {{
+        WITH n
+        DETACH DELETE n
+    }} IN TRANSACTIONS OF {batch_size} ROWS
+    """
+    session.run(query)
 connector = Neo4jConnector()
 
 # 1. Resolve matching nodes purely using the Blueprint configuration
+##TODO change merged back to all uppercase letter filter and MAERGEND INTO TO MAPPED_TO
+for blue_print_class in [ MergedProteinDomain, MergedTerm, MergedDisease, MergedTissue, MergedPTM]:  # Add more Blueprint classes as needed
+    with connector.driver.session() as session:
+        delete_label_in_batches(session, blue_print_class.__label__, 10000)
 
-with connector.driver.session() as session:
-    created_merged_labels = session.run("MATCH (n) WHERE labels(n)[0] CONTAINS 'Merged' RETURN apoc.coll.flatten(COLLECT(DISTINCT labels(n))) as mergedLabels").single()["mergedLabels"] 
-
-for blue_print_class in [ MergedProtein, MergedGene, MergedPublication, MergedProteinDomain, MergedRna, MergedTaxon, MergedTerm, MergedDisease, MergedTissue, MergedPTM]:  # Add more Blueprint classes as needed
     resolver = EntityResolver(connector)
-    if blue_print_class.__label__ in created_merged_labels: continue
     resolver.process_graph(blueprint_class=blue_print_class)
 
     # 2. Run the Pydantic preprocessing and integrate them into Neo4j
@@ -34,4 +36,4 @@ for blue_print_class in [ MergedProtein, MergedGene, MergedPublication, MergedPr
     )
 connector.close()
 
-## TODO: Disease and tissue looks weird (some property toll up did not work properly) + pubmed ids connection to HPRD_PTM to Publication -> We need a link specific entity resolver one that creates a link not a new node based on a shared entity
+## TODO: New connections within data -> missing Mapping connections or linkings in the data? Need to check the properties and their values
