d81b539374eb08bda4d75a17e74908fce2211611
Daniel Walke <daniel.walke@ovgu.de>
2026-04-30 09:37:09 +0200

    has ontology edge for ptms

--- mapping/linking/main.py
+++ mapping/linking/main.py
@@ -1,6 +1,7 @@
 from mapping.integrations.connector import Neo4jConnector
 from mapping.linking.entity_linker import EntityLinker
 from mapping.linking.references_edge import ReferencesPTMEdge
+from mapping.linking.has_ontology_edge import HasOntologyEdge
 
 
 def main():
@@ -11,6 +12,7 @@ def main():
         # List out all the specific edge definitions you want to run
         edges_to_process = [
             ReferencesPTMEdge(),
+            HasOntologyEdge(),
             # ProteinToGeneEdge(),
             # MetaboliteToPathwayEdge(),
         ]
