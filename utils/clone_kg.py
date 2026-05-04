ac385fd30a565691b775947014fc7542b82289ee
Daniel Walke <daniel.walke@ovgu.de>
2026-05-04 12:33:37 +0200

    fixed clone bug

--- utils/clone_kg.py
+++ utils/clone_kg.py
@@ -4,13 +4,25 @@ import shutil
 import sys
 import glob
 
+def get_dir_size(path='.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+    return total_size
+
+def format_bytes(size):
+    for unit in ['B', 'K', 'M', 'G', 'T']:
+        if size < 1024:
+            return f"{size:.1f}{unit}"
+        size /= 1024
+
 
 def clone_kg(source_data_dir, target_project_dir):
     IS_WINDOWS = platform.system() == "Windows"
     IS_MACOS = platform.system() == "Darwin"
-    if not IS_WINDOWS and os.geteuid() != 0:
-        print("Run with sudo (required on Linux/macOS to set file ownership)")
-        sys.exit(1)
 
     os.makedirs(target_project_dir, exist_ok=True)
     target_data_dir = os.path.join(target_project_dir, 'data')
@@ -19,7 +31,9 @@ def clone_kg(source_data_dir, target_project_dir):
         shutil.rmtree(target_data_dir)
 
     shutil.copytree(source_data_dir, target_data_dir)
-
+    print(f"Copied data from {source_data_dir} to {target_data_dir}")
+    print(os.listdir(target_data_dir))
+    print(format_bytes(get_dir_size(target_data_dir)))
     for pattern in ['**/database_lock', '**/store_lock', '**/*.tmp.*', '**/*.tmp']:
         for f in glob.glob(os.path.join(target_data_dir, pattern), recursive=True):
             os.remove(f)
@@ -28,21 +42,19 @@ def clone_kg(source_data_dir, target_project_dir):
     directories_to_fix = ['data', 'logs', 'conf', 'import']
 
     for dir_name in directories_to_fix:
+        print(f"Ensuring directory exists and setting permissions: {dir_name}")
         dir_path = os.path.join(target_project_dir, dir_name)
         os.makedirs(dir_path, exist_ok=True)
 
         if not IS_WINDOWS:
             for root, dirs, files in os.walk(dir_path):
-                os.chown(root, 7474, 7474)
                 if IS_MACOS:
                     os.chmod(root, 0o777)
                 for d in dirs:
                     path = os.path.join(root, d)
-                    os.chown(path, 7474, 7474)
                     if IS_MACOS:
                         os.chmod(path, 0o777)
                 for f in files:
                     path = os.path.join(root, f)
-                    os.chown(path, 7474, 7474)
                     if IS_MACOS:
                         os.chmod(path, 0o666)
