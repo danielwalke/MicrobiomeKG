3bcf8433c7c31a5176c82c535808850f299abce3
Daniel Walke <daniel.walke@ovgu.de>
2026-05-04 12:36:39 +0200

    fixed path bug in cloning (expad user path to python)

--- s4_filtered_rolledup_graph/clone_kg.py
+++ s4_filtered_rolledup_graph/clone_kg.py
@@ -3,5 +3,5 @@ import os
 if __name__ == "__main__":
     source_dir = "~/mnt/client_data/mikrobiome_kg/workspace/neo4j/neo4j.db/data/"
     source_dir = os.path.expanduser(source_dir)
-    target_dir = "s4_filtered_rolledup_graph/"
+    target_dir = "~/mnt/client_data/mikrobiome_kg/s4_filtered_rolledup_graph/"
     clone_kg(source_dir, target_dir)
--- s7_postfiltered_graph/clone_kg.py
+++ s7_postfiltered_graph/clone_kg.py
@@ -1,6 +1,9 @@
 from utils.clone_kg import clone_kg
+import os
 
 if __name__ == "__main__":
-    source_dir = "s4_filtered_rolledup_graph/data/"
-    target_dir = "s7_postfiltered_graph/"
+    source_dir = "~/mnt/client_data/mikrobiome_kg/s4_filtered_rolledup_graph/data/"
+    target_dir = "~/mnt/client_data/mikrobiome_kg/s7_postfiltered_graph/"
+    source_dir = os.path.expanduser(source_dir)
+    target_dir = os.path.expanduser(target_dir)
     clone_kg(source_dir, target_dir)
