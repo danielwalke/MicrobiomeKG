c74464051becc2c60c5d01b2a0706c03d89b2e7e
Daniel Walke <daniel.walke@ovgu.de>
2026-05-06 10:53:54 +0200

    correct kg clonign

--- s4_filtered_rolledup_graph/clone_kg.py
+++ s4_filtered_rolledup_graph/clone_kg.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     source_dir = "~/mnt/client_data/mikrobiome_kg/workspace/neo4j/neo4j.db/data/"
     source_dir = os.path.expanduser(source_dir)
     target_dir = "~/mnt/client_data/mikrobiome_kg/s4_filtered_rolledup_graph/"
+    target_dir = os.path.expanduser(target_dir)
     clone_kg(source_dir, target_dir)
